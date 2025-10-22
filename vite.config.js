ae2f493668f705203ebdb1b58c38c6519aa6da78
VLADISLAV NACHIKOV <vlnachikov@gmail.com>
2025-10-22 13:10:33 +0200

    Ad ci-cd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? "/ReactChec/" : "/",
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.js",
+  },
 });
